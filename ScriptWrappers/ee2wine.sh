15d227750b51612bd832ebd7b75704146aba4a50
Erez Binyamin <>
2024-11-27 15:57:26 -0500

    ee2wine: Splash screen added, Dockerfile improved with gdown google drive links, bashrc exports wine envars

--- ScriptWrappers/ee2wine.sh
+++ ScriptWrappers/ee2wine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Functions that will launch docker containers
 ee2wine() { 	
-	local IMAGE_NAME=erezbinyamin/ee2wine:latest
+	local IMAGE_NAME=${IMAGE_NAME:-erezbinyamin/ee2wine:latest}
 	if [ "${USER_VOLUME}" == "winehome" ] && ! docker volume ls -qf "name=winehome" | grep -q "^winehome$"
 	then
 		echo "INFO: Creating Docker volume container 'winehome'..."
@@ -16,32 +16,25 @@ ee2wine() {
 	xhost +local:$(id -un)
 	xauth list "${DISPLAY}" | head -n1 | awk '{print $3}' > ~/.docker-wine.Xkey
 
-	printf ' 
-
-	# Play the game
-	cd "/root/.PlayOnLinux/wineprefix/Games/drive_c/Program Files/Empire Earth/Empire Earth"
-	WINEPREFIX="$HOME/.PlayOnLinux/wineprefix/Games/" WINEARCH=win32 WINE="/opt/wine-staging/bin/wine" LIBGL_ALWAYS_SOFTWARE=1 wine "./Empire Earth.exe"
-
-	'
-	
-	sudo chrt --fifo 99 docker run -it --rm \
-		--name=wine2 \
-		--hostname=$(hostname) \
-		--shm-size=1g \
-		--workdir=/ \
-		--env=RUN_AS_ROOT=yes \
-		--env=DISPLAY \
-		--env=TZ=America/New_York \
-		--volume=/home/${USER}/.docker-wine.Xkey:/root/.Xkey:ro \
-		--volume=/tmp/pulse-socket:/tmp/pulse-socket \
-		--volume=/tmp/.X11-unix:/tmp/.X11-unix:ro \
-		--volume=winehome:/home/wineuser \
-		--cap-add=NET_ADMIN \
-		--net=host \
-		--privileged \
-		${IMAGE_NAME} bash &>/dev/null
-
-	if [ $? -eq 125 ]
+	if false && [ -e /tmp/pulse-socket ]
+	then
+		sudo chrt --fifo 99 docker run -it --rm \
+			--name=wine2 \
+			--hostname=$(hostname) \
+			--shm-size=1g \
+			--workdir=/ \
+			--env=RUN_AS_ROOT=yes \
+			--env=DISPLAY \
+			--env=TZ=America/New_York \
+			--volume=/home/${USER}/.docker-wine.Xkey:/root/.Xkey:ro \
+			--volume=/tmp/pulse-socket:/tmp/pulse-socket \
+			--volume=/tmp/.X11-unix:/tmp/.X11-unix:ro \
+			--volume=winehome:/home/wineuser \
+			--cap-add=NET_ADMIN \
+			--net=host \
+			--privileged \
+			${IMAGE_NAME} bash &>/dev/null
+	elif [ -d /tmp/.X11-unix ]
 	then
 		sudo chrt --fifo 99 docker run -it --rm \
 			--name=wine2 \
